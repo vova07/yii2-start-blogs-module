b23340c07f09ab5b28c6111fa018a94200fc94d9
vova07 <bazillio07@yandex.ru>
2015-03-01 15:29:38 +0200

    Add 'de' language.

--- messages/config.php
+++ messages/config.php
@@ -3,7 +3,7 @@
 return [
     'sourcePath' => dirname(__DIR__),
     'messagePath' => __DIR__,
-    'languages' => ['en', 'ru'],
+    'languages' => ['de', 'en', 'pt-BR', 'ru'],
     'translator' => 'Module::t',
     'sort' => false,
     'overwrite' => true,
