9d6d2797f33b0cd6671471de7dbc602a4904edc9
vova07 <bazillio07@yandex.ru>
2015-03-10 01:08:52 +0200

    Small fix for view config.

--- Module.php
+++ Module.php
@@ -113,10 +113,13 @@ class Module extends \yii\base\Module
     {
         parent::init();
 
-        if ($this->getIsBackend() === true) {
-            $this->setViewPath('@vova07/blogs/views/backend');
-        } else {
-            $this->setViewPath('@vova07/blogs/views/frontend');
+        // @todo Refactor this line if https://github.com/yiisoft/yii2/issues/7647 will be solved.
+        if ($this->getViewPath() === $this->getBasePath() . DIRECTORY_SEPARATOR . 'views') {
+            if ($this->getIsBackend() === true) {
+                $this->setViewPath('@vova07/blogs/views/backend');
+            } else {
+                $this->setViewPath('@vova07/blogs/views/frontend');
+            }
         }
     }
 
